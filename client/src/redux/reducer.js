a080df1450f4b0edc13fae61f1fcb9e2c03f54f6
Gonzalo Schmidt <gonza.sch35@gmail.com>
2023-11-09 18:30:58 -0300

    vite app client

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,16 @@
+
+
+let initialState = {
+    productos: [],
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return{
+                ...state
+            }
+    }
+}
+
+export default rootReducer

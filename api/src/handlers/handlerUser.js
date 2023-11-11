69ba7fa84ca048492a787c2ca2e862cca225cb4e
Gonzalo Schmidt <gonza.sch35@gmail.com>
2023-11-10 21:26:19 -0300

    handler users

--- api/src/handlers/handlerUser.js
+++ api/src/handlers/handlerUser.js
@@ -1,18 +1,24 @@
-
+const {User} = require('../db')
 
 const getAllUsers = async (req,res) => {
     try {
-        
+        const users = await User.findAll()
+        if(!users) throw Error('No hay usuarios registrados')
+        res.status(200).json(users)
     } catch (error) {
-        
+        res.status(400).json({error: error.message})
     }
 }
 
 const postUser = async (req,res) => {
+    const {name, email, password, phone, address} = req.body
     try {
-        
+        const findOne = await User.findAll({where: {email}})
+        if(findOne) throw Error('El email ingresado ya esta registrado')
+        await User.create({name, email, password, phone, address})
+        res.status(200).json('Usuario creado con exito')
     } catch (error) {
-        
+        res.status(400).json({error: error.message})
     }
 }
 

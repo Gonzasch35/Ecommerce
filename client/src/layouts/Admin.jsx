2c9865f6336a5e68758dd8bdb12a568c3ed23bce
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2023-12-27 10:09:31 -0300

    hola

--- client/src/layouts/Admin.jsx
+++ client/src/layouts/Admin.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom"
 
 const Admin = () => {
   return (
+    //hola
     <>
         <main className="bg-gradient-to-br from-cyan-900 to-green-700 min-h-screen">
             <Outlet />

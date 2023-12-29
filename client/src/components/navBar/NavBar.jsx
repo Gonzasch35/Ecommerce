684ed4a4f8056a8bc905021c9a3f64c20c295e5f
Gonzalo Schmidt <gonza.sch35@gmail.com>
2023-12-29 11:17:21 -0300

    feat/cart

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -9,8 +9,9 @@ const NavBar = ({categorias}) => {
 
   const user = useSelector(state=>state.user)
   const dispatch = useDispatch()
+  const token = localStorage.getItem('token')
 
-const handleClick = (id) => {
+  const handleClick = (id) => {
     dispatch(filter_product(id))
   }
 
@@ -59,11 +60,12 @@ const handleClick = (id) => {
           </ul>
         </div>
         <div className='flex gap-3 justify-center items-center'>
+          <Link to='/cart'>Carrito</Link>
           <img src={heart} alt="" />
               {user.id ? 
-              <a href="/" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cerrar sesión</a>
+              <Link href="/" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Cerrar sesión</Link>
                 : 
-                <a href="/login" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Iniciar sesión</a>
+                <Link href="/login" class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Iniciar sesión</Link>
               }
         </div>
       </div>
--- client/src/pages/login/ConfirmAccount.jsx
+++ client/src/pages/login/ConfirmAccount.jsx
@@ -41,7 +41,7 @@ const ConfirmAccount = () => {
                         </h1>}
                 <Link
                     class="mt-6 block select-none rounded-lg bg-violet-500 hover:bg-violet-700 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-violet-500/20 transition-all hover:shadow-lg hover:shadow-violet-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                    to='/'
+                    to='/login'
                 >
                     Iniciar Sesión
                 </Link>
